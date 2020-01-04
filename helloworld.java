afec567124e84ceec0b87c8eb95dc7a94c2b7479
sanjaygeeky <49758153+sanjaygeeky@users.noreply.github.com>
2020-01-04 15:05:53 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+/* This is sample code for Jenkins Hello World */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World 2020 A decade and year :) !");
+    }
+}
