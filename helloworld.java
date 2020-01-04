1c36f20fff97aaa949658d42fccd40ff1d994d3a
Sanjay Mahajan <49758153+sanjaygeeky@users.noreply.github.com>
2020-01-04 16:49:09 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 /* This is sample code for Jenkins Hello World */
 
-public class HelloWorld {
+public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World 2020 A decade and year :) !");
     }
